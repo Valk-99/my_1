fc5a46dacc3fb6862ddcdcc85941c7aa5bb03656
Valentin <valentinmirosnik0@gmail.com>
2021-03-26 07:50:52 +0200

    some changes

--- mkdev/main/views.py
+++ mkdev/main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    turn_on_block = False
+    turn_on_block = True
     return render(request, 'main/index.html', {'turn_on_block': turn_on_block})
