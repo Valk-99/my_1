a4546a90ad3acc5dd64596c152d823046e6ce215
Valentin <valentinmirosnik0@gmail.com>
2021-03-26 08:40:24 +0200

    #48 commit

--- mkdev/main/views.py
+++ mkdev/main/views.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 
 def index(request):
     turn_on_block = True
-    return render(request, 'main/index.html', {'turn_on_block': turn_on_block})
+    now = datetime.now()
+    return render(request, 'main/index.html', {'turn_on_block': turn_on_block, 'now': now})
