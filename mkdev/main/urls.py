8d79da30ba797edcdc73ae17c9e15c35347b07ef
Valentin <valentinmirosnik0@gmail.com>
2021-03-26 09:46:51 +0200

    Add #47 commit

--- mkdev/main/urls.py
+++ mkdev/main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.contrib.flatpages import views as viewsf
 
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexPageListView.as_view(), name='index'),
+    path('good/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+
+    # Flatpages
     path('main/', viewsf.flatpage, {'url': '/main/'}, name='main'),
 ]
--- mkdev/main/views.py
+++ mkdev/main/views.py
@@ -1,9 +1,29 @@
 from datetime import datetime
 
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from main.models import Product
 
 
-def index(request):
-    turn_on_block = True
-    now = datetime.now()
-    return render(request, 'main/index.html', {'turn_on_block': turn_on_block, 'now': now})
+class IndexPageListView(ListView):
+    model = Product
+    template_name = 'main/index.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['turn_on_block'] = True
+        context['now'] = datetime.now()
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'main/product_detail.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['turn_on_block'] = True
+        context['now'] = datetime.now()
+        return context
