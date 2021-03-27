cb78d5b8179c9ad2d20d453650995dc4d9ade670
Valentin <valentinmirosnik0@gmail.com>
2021-03-27 19:09:28 +0200

    add test

--- mkdev/main/forms.py
+++ mkdev/main/forms.py
@@ -1,12 +1,23 @@
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+from main.models import Profile
 
 
 class ProfileForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    age = forms.IntegerField()
+
+    def validate_age(self, age):
+        if age <= 18:
+            raise ValidationError(_('%(age) should be more than 18'),
+                                  params={'age': age}, )
 
     class Meta:
-        model = User
-        fields = ['email', 'first_name', 'last_name']
+        model = Profile
+        fields = ['email', 'first_name', 'last_name', 'age']
