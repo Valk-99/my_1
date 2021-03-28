69ae1cca8752ec377069af991a51c5306b569043
Valentin <valentinmirosnik0@gmail.com>
2021-03-28 11:09:12 +0300

    Add inline in admin

--- mkdev/main/admin.py
+++ mkdev/main/admin.py
@@ -29,4 +29,18 @@ admin.site.register(Customer)
 admin.site.register(Tag)
 admin.site.register(Seller)
 admin.site.register(Order)
-admin.site.register(Profile)
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
