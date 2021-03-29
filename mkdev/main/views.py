7a744d169790fa3e87e8bf5f013eda8f8bb6556c
Valentin <valentinmirosnik0@gmail.com>
2021-03-29 13:58:06 +0300

    some changes

--- mkdev/main/views.py
+++ mkdev/main/views.py
@@ -42,6 +42,7 @@ class ProductByTagListView(ListView):
 
     def get_context_data(self, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['now'] = datetime.now()
         context['Tag'] = Tag.objects.all()
         context['tag_title'] = Tag.objects.get(slug=self.kwargs['tag_slug'])
         return context
