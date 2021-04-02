7d79e4f288c83c3042d9c03fcfb77e4596cd8c6a
Valentin <valentinmirosnik0@gmail.com>
2021-04-02 11:08:30 +0300

    Add install celery

--- mkdev/mkdev/settings.py
+++ mkdev/mkdev/settings.py
@@ -148,3 +148,13 @@ SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
+
+# Celery settings
+
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost'
+
+#: Only add pickle to this list if your broker is secured
+#: from unwanted access (see userguide/security.html)
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+CELERY_TASK_SERIALIZER = 'json'
