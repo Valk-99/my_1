a2c97f689727221cbe3c96752e94881c67e9f4b9
Valentin <valentinmirosnik0@gmail.com>
2021-04-07 09:12:36 +0300

    Add view cache

--- mkdev/main/urls.py
+++ mkdev/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.flatpages import views as viewsf
+from django.views.decorators.cache import cache_page
 
 from .views import IndexPageListView, ProductDetailView, ProductByTagListView, ProfileUpdate, ProfileCreate, \
     ProductUpdate, CreateProduct
@@ -8,7 +9,7 @@ urlpatterns = [
     path('', IndexPageListView.as_view(), name='index'),
     path('good/add/', CreateProduct.as_view(), name='product_create'),
     path('good/<int:pk>/edit/', ProductUpdate.as_view(), name='product_update'),
-    path('good/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('good/<int:pk>/', cache_page(60 * 15)(ProductDetailView.as_view()), name='product_detail'),
     path('good/<slug:tag_slug>/', ProductByTagListView.as_view(), name='tag_slug'),
 
     path('accounts/profile/create/', ProfileCreate.as_view(), name='profile_form'),
--- mkdev/mkdev/settings.py
+++ mkdev/mkdev/settings.py
@@ -73,22 +73,19 @@ MIDDLEWARE = [
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware', # cache
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware', # cache
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # НОВОЕ
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
-# Cache
+# # Cache
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = '600'
 CACHE_MIDDLEWARE_KEY_PREFIX = ''
-CACHE_TTL = 60 * 5
 
 CACHES = {
     'default':  {
