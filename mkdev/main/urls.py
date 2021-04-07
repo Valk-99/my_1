1ca4920ba3b26436d5ad43466d403b5352186eda
Valentin <valentinmirosnik0@gmail.com>
2021-04-07 13:11:41 +0300

    #

--- mkdev/main/urls.py
+++ mkdev/main/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', cache_page(CACHE_TTL)(IndexPageListView.as_view()), name='index'),
     path('good/add/', CreateProduct.as_view(), name='product_create'),
     path('good/<int:pk>/edit/', ProductUpdate.as_view(), name='product_update'),
-    path('good/<int:pk>/',cache_page(CACHE_TTL)(ProductDetailView.as_view()), name='product_detail'),
+    path('good/<int:pk>/',ProductDetailView.as_view(), name='product_detail'),
     path('good/<slug:tag_slug>/', ProductByTagListView.as_view(), name='tag_slug'),
 
     path('accounts/profile/create/', ProfileCreate.as_view(), name='profile_form'),
