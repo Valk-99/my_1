f40b3a9299b9da2174bedc04a0807de17b0ca3e2
Valentin <valentinmirosnik0@gmail.com>
2021-04-10 20:40:34 +0300

    test

--- mkdev/main/tests/test_views.py
+++ mkdev/main/tests/test_views.py
@@ -30,87 +30,98 @@ class ViewGetPostTestCase(TestCase):
         self.profile_create_url = reverse('profile_form')
         self.profile_update_url = reverse('profile', kwargs={'pk': self.profile.pk})
         self.create_product_url = reverse('product_create')
-        self.product_update_url = reverse('product_update', kwargs={'pk': 1})
 
-    def test_index_list_view_url_get(self):
-        resp = self.client.get(self.index_url)
-        self.assertEqual(resp.status_code, 200)
+#     def test_index_list_view_url_get(self):
+#         resp = self.client.get(self.index_url)
+#         self.assertEqual(resp.status_code, 200)
+#
+#     def test_product_detail_view_url_get(self):
+#         resp = self.client.get(self.product_detail_url)
+#         self.assertEqual(resp.status_code, 200)
+#
+#     def test_tag_detail_view_url_get(self):
+#         resp = self.client.get(self.tag_detail_url)
+#         self.assertEqual(resp.status_code, 200)
+#
+#     def test_profile_create_view_url_get(self):
+#         self.client.login(username='john', password='johnpassword')
+#         resp = self.client.get(self.profile_create_url)
+#         self.assertEqual(resp.status_code, 200)
+#
+#     def test_profile_create_view_url_post(self):
+#         self.client.login(username='john', password='johnpassword')
+#         self.user2 = User.objects.create_user('john1', 'lennon@thebeatles.com', 'johnpassword')
+#         resp = self.client.post(self.profile_create_url, {'user_profile': 2, 'how_old': 22})
+#         self.assertEqual(resp.status_code, 302)
+#
+#     def test_profile_update_view_url_get(self):
+#         self.client.login(username='john', password='johnpassword')
+#         resp = self.client.get(self.profile_update_url)
+#         self.assertEqual(resp.status_code, 200)
+#
+#     def test_profile_update_view_url_post(self):
+#         self.client.login(username='john', password='johnpassword')
+#         resp = self.client.post(self.profile_update_url, {'user_profile': 1, 'how_old': 22})
+#         self.assertEqual(resp.status_code, 302)
+#         self.profile.refresh_from_db()
+#         self.assertEqual(self.profile.how_old, 22)
+#
+#     def test_create_product_view_url_get(self):
+#         self.client.login(username='john', password='johnpassword')
+#         self.user.user_permissions.add(self.permission)
+#         resp = self.client.get(self.create_product_url)
+#         self.assertEqual(resp.status_code, 200)
+#
+#     def test_product_create_view_url_post(self):
+#         self.client.login(username='john', password='johnpassword')
+#         self.user.user_permissions.add(self.permission)
+#         self.user3 = User.objects.create_user('john3', 'lennon@thebeatles.com', 'johnpassword')
+#         resp = self.client.post(self.create_product_url, {
+#             'category': Category.objects.create(name='Apple2', slug='apple2'),
+#             'seller': Seller.objects.create(user=self.user3, name='john'),
+#             'title':  'iPhone 22',
+#             'slug':  'iphone-22',
+#             'description': "Heeeeeey",
+#             'price': 23.00,
+#             'is_active': True,
+#             'views': 1,
+#         })
+#         self.assertEqual(resp.status_code, 302)
+#
+#     def test_product_update_view_url_get(self):
+#         self.client.login(username='john', password='johnpassword')
+#         self.user.user_permissions.add(self.permission1)
+#         resp = self.client.get(self.product_update_url)
+#         self.assertEqual(resp.status_code, 200)
 
-    def test_product_detail_view_url_get(self):
-        resp = self.client.get(self.product_detail_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_tag_detail_view_url_get(self):
-        resp = self.client.get(self.tag_detail_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_profile_create_view_url_get(self):
-        self.client.login(username='john', password='johnpassword')
-        resp = self.client.get(self.profile_create_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_profile_create_view_url_post(self):
-        self.client.login(username='john', password='johnpassword')
-        self.user2 = User.objects.create_user('john1', 'lennon@thebeatles.com', 'johnpassword')
-        resp = self.client.post(self.profile_create_url, {'user_profile': 2, 'how_old': 22})
-        self.assertEqual(resp.status_code, 302)
-
-    def test_profile_update_view_url_get(self):
-        self.client.login(username='john', password='johnpassword')
-        resp = self.client.get(self.profile_update_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_profile_update_view_url_post(self):
-        self.client.login(username='john', password='johnpassword')
-        resp = self.client.post(self.profile_update_url, {'user_profile': 1, 'how_old': 22})
-        self.assertEqual(resp.status_code, 302)
-        self.profile.refresh_from_db()
-        self.assertEqual(self.profile.how_old, 22)
-
-    def test_create_product_view_url_get(self):
-        self.client.login(username='john', password='johnpassword')
-        self.user.user_permissions.add(self.permission)
-        resp = self.client.get(self.create_product_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_product_create_view_url_post(self):
-        self.client.login(username='john', password='johnpassword')
-        self.user.user_permissions.add(self.permission)
+    def test_product_update_view_url_post(self):
+        self.client = Client()
         self.user3 = User.objects.create_user('john3', 'lennon@thebeatles.com', 'johnpassword')
-        resp = self.client.post(self.create_product_url, {
-            'category': Category.objects.create(name='Apple2', slug='apple2'),
-            'seller': Seller.objects.create(user=self.user3, name='john'),
-            'title':  'iPhone 22',
-            'slug':  'iphone-22',
+        self.client.login(username='john3', password='johnpassword')
+        self.permission1 = Permission.objects.get(name='Can change product')
+        self.user3.user_permissions.add(self.permission1)
+        self.product1 = Product.objects.create(
+            category=Category.objects.create(name='Apple2', slug='apple2'),
+            seller=Seller.objects.create(user=self.user3, name='john3'),
+            title='iPhone 20',
+            slug='iphone-22',
+            description="Heeeeeey",
+            price=23.00,
+            is_active=True,
+            views=1,)
+        resp = self.client.post(reverse('product_update', kwargs={'pk': self.product1.id}), {
+            'category': 'Apple2',
+            'seller': 'john2',
+            'title': 'iPhone 220',
+            'slug': 'iphone-22',
             'description': "Heeeeeey",
             'price': 23.00,
             'is_active': True,
             'views': 1,
         })
         self.assertEqual(resp.status_code, 302)
-
-    def test_product_update_view_url_get(self):
-        self.client.login(username='john', password='johnpassword')
-        self.user.user_permissions.add(self.permission1)
-        resp = self.client.get(self.product_update_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_product_update_view_url_post(self):
-        self.client.login(username='john', password='johnpassword')
-        self.user.user_permissions.add(self.permission1)
-        resp = self.client.post(self.product_update_url, {
-            'category': self.product.category,
-            'seller': self.product.seller,
-            'title': self.product.title,
-            'slug': self.product.slug,
-            'description': "Heeeeeey123",
-            'price': self.product.price,
-            'is_active': self.product.is_active,
-            'views': self.product.views,
-        })
-        self.assertEqual(resp.status_code, 302)
-        self.product.refresh_from_db()
-        self.assertEqual(self.product.description, 'Heeeeeey123')
+        self.product1.refresh_from_db()
+        self.assertEqual(self.product1.title, 'iPhone 220')
 
 
 
